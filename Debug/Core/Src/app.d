048ff9aec4b9de0ece90ec8d59209a20d7faa9cf
Jorge Ricarte <jorgericartepg@gmail.com>
2024-04-18 12:13:33 -0300

    removed adc pin config from ioc

--- Debug/Core/Src/app.d
+++ Debug/Core/Src/app.d
@@ -18,8 +18,6 @@ Core/Src/app.o: ../Core/Src/app.cpp ../Core/Inc/app.hpp \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
- ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h \
- ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
@@ -54,8 +52,6 @@ Core/Src/app.o: ../Core/Src/app.cpp ../Core/Inc/app.hpp \
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
-../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h:
-../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h:
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
